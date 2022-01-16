9eab0679f6f6e7b0f92b9fc3ee507e6315f29e40
Richard Havalda <havalda.r94@gmail.com>
2022-01-16 23:30:48 +0100

    feat:  add header component with styles

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
+import Header from './Header';
+
 function App() {
-  const title = 'Blog post';
-  const body = 'This is my blog post';
-  const comments = [
-    { id: 1, text: 'comment one' },
-    { id: 2, text: 'comment two' },
-    { id: 3, text: 'comment three' },
-  ];
-
-  const loading = false;
-  const showComments = true;
-  const commentBlock = (
-    <div className='comments'>
-      <h3>Comments {comments.length}</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
-  if (loading) return <h1>Loading...</h1>;
-
   return (
-    <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   );
-
-  //   return React.createElement(
-  //     'div',
-  //     { className: 'container' },
-  //     React.createElement('h1', {}, 'My App')
-  //   );
 }
 
 export default App;
