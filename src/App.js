d46cad59b5b76231a798fd2b509235b89d83a282
Richard Havalda <havalda.r94@gmail.com>
2022-01-16 22:32:32 +0100

    feat: reinitialize app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return <h1>Hello from the app component</h1>;
+
+  //   return React.createElement(
+  //     'div',
+  //     { className: 'container' },
+  //     React.createElement('h1', {}, 'My App')
+  //   );
+}
+
+export default App;
