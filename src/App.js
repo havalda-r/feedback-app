385f4624dede3fb2767576aae82bac839c45988e
Richard Havalda <havalda.r94@gmail.com>
2022-01-16 23:15:26 +0100

    feat: add conditionals

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ function App() {
     { id: 3, text: 'comment three' },
   ];
 
+  const loading = false;
+  const showComments = true;
+  const commentBlock = (
+    <div className='comments'>
+      <h3>Comments {comments.length}</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div className='container'>
       <h1>{title}</h1>
       <p>{body}</p>
-      <div className='comments'>
-        <h3>Comments {comments.length}</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      {showComments && commentBlock}
     </div>
   );
 
