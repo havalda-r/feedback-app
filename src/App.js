8ad31001746e3cd715802f81e736ae7e2ccf6b4d
Richard Havalda <havalda.r94@gmail.com>
2022-01-16 22:40:40 +0100

    feat: test dynamic values and list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const title = 'Blog post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'comment one' },
+    { id: 2, text: 'comment two' },
+    { id: 3, text: 'comment three' },
+  ];
+
+  return (
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className='comments'>
+        <h3>Comments {comments.length}</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 
   //   return React.createElement(
   //     'div',
