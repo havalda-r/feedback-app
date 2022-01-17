2d72433885494f85e778e0bdb9aa9d3ccc104390
Richard Havalda <havalda.r94@gmail.com>
2022-01-17 14:32:06 +0100

    feat: add custom button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import Card from './shared/Card';
 import { useState } from 'react';
+import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
@@ -19,7 +20,7 @@ function FeedbackForm() {
             value={text}
             onChange={handleTextChange}
           ></input>
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
