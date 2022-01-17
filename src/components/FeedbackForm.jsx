bf061714462c8b89fb6dc73cb9b183f612009011
Richard Havalda <havalda.r94@gmail.com>
2022-01-17 14:21:10 +0100

    feat: initialize form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,29 @@
+import Card from './shared/Card';
+import { useState } from 'react';
+
+function FeedbackForm() {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would your rate your service with us?</h2>
+        <div className='input-group'>
+          <input
+            type='text'
+            placeholder='Write a review!'
+            value={text}
+            onChange={handleTextChange}
+          ></input>
+          <button type='submit'>Send</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
